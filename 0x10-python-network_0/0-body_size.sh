89a2e32c9976a55cd0fc5786cdc5273cf5500b66
K-kare <kamunawangari@gmail.com>
2024-04-25 12:01:17 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -6,3 +6,9 @@ if [ $# -eq 0 ]; then
 fi
 url="$1"
 response=$(curl -s -o /tmp/curl_response.tmp -w "%{size_download}" "$url")
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Size of the response body: $response bytes"
+
