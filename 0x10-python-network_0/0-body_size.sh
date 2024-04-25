622c23ea286c03e7b05b4d61fd2e11e2924a27ab
K-kare <kamunawangari@gmail.com>
2024-04-25 12:16:49 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -5,7 +5,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 url="$1"
-response=$(curl -s -o /tmp/curl_response.tmp -w "%{size_download}" "$url")
+response=$(curl -s -w "%{size_download}" "$url")
 if [ $? -ne 0 ]; then
     exit 1
 fi
