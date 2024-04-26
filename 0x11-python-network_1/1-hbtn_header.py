47ff344540df5982c0bc8820981cf573237b8d8d
K-kare <kamunawangari@gmail.com>
2024-04-26 07:18:11 -0700

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Python script that takes in a URL, 
+#sends a request to the URL and displays the value of the X-Request-Id
+import urllib
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print("X-Request-Id:", x_request_id)
+        else:
+            print("X-Request-Id header not found in the response.")
+
