8bbf285ba45d6691ea0d3c2308a60956b455cd2a
K-kare <kamunawangari@gmail.com>
2024-04-26 04:40:45 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read().decode('utf-8')
-    print("\t- Body response:")
+    print("Body response:")
     print("\t\t- type:", type(body))
     print("\t\t- content:", body)
-    print("\t- utf8 content:", body)
+    print("\t\t- utf8 content:", body)
