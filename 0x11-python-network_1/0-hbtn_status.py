cb7738e112288e51651bf29c45597fb7b8ca71ce
K-kare <kamunawangari@gmail.com>
2024-04-26 04:05:36 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,3 +3,6 @@
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
+    print("\t- Body response:")
+    print("\t\t- type:", type(body))
+    print("\t\t- content:", body)
