59392810786700e9151136c58f9363737f278627
K-kare <kamunawangari@gmail.com>
2024-04-26 08:39:30 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ import sys
 if len(sys.argv) < 2:
     print("Usage: python script.py <URL>")
     sys.exit(1)
+url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         x_request_id = response.headers.get('X-Request-Id')
         if x_request_id:
