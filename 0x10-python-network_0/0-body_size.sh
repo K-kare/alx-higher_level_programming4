cbcc8def05a6718b87a218a008c84c68b41c8d50
K-kare <kamunawangari@gmail.com>
2024-04-30 02:42:08 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL
 # displays the size of the body of the response
-if [ -z "$1" ]; then
-    exit 1
-fi
 response=$(curl -s  -w "%{size_download}" "$1")
 echo $response
-
